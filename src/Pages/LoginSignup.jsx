02f18fe52214ebd140c29d756e9f25d73ebfcc3a
Pratyush271299 <lightyagamikilledl@gmail.com>
2024-06-05 19:06:49 +0530

    update

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -2,8 +2,23 @@ import React from 'react';
 
 const LoginSignup = () => {
   return (
-    <div>
-      
+    <div className='loginsignup'>
+      <div className="loginsignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginsign-fields">
+          <input type="text" placeholder='Your Name' />
+          <input type="email" placeholder='Email Address' />
+          <input type="password" placeholder='Password' />
+        </div>
+        <button>Continue</button>
+        <p className='loginsignup-login'>
+          Already have an account? <span>Login Here</span>
+        </p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name='' id='' />
+          <p>By Continuing, I agree to use the terms of use & privacy policy.</p>
+        </div>
+      </div>
     </div>
   );
 }

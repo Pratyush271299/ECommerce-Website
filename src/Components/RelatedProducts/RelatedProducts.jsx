f51429461f3d94453cb00a9aeac4693c08bf7c90
Pratyush271299 <lightyagamikilledl@gmail.com>
2024-06-07 12:19:20 +0530

    update

---
+++ src/Components/RelatedProducts/RelatedProducts.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './RelatedProducts.css'
+const RelatedProducts = () => {
+  return (
+    <div className='realtedproducts'>
+      
+    </div>
+  );
+}
+
+export default RelatedProducts;
